5cc09b56ee4771e9439dc5f33b5f72d2f5e4ef74
agung-n <agung.nugraha@live.com>
2021-11-13 15:28:06 +0000

    Initial commit

---
+++ resources/views/post.blade.php
@@ -0,0 +1,18 @@
+<title>My Blog</title>
+<link rel="stylesheet" href="/app.css">
+
+<body>
+    <article>
+        <?= $post; ?>
+    </article>
+    <a href="/posts">Go Back</a>
+</body>
+
+
+
+
+
+
+
+
+
